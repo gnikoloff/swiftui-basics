e1c48fec09d353e3be58faf3d149b5d9c55e0713
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-02-02 22:39:17 +0100

    Initial Commit

---
+++ flagguesser/flagguesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  flagguesserApp.swift
+//  flagguesser
+//
+//  Created by Georgi Nikolov on 02.02.22.
+//
+
+import SwiftUI
+
+@main
+struct flagguesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
