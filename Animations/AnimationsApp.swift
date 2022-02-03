3a0084a6fbc92ef80736e03bc7bcfb751b3b7fda
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-02-03 21:01:39 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Georgi Nikolov on 03.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
