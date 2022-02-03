8bb7540926b5367de76814e3a47baf5ed4220493
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-02-03 13:00:39 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Georgi Nikolov on 03.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
