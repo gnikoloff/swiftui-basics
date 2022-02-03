4e16f0ff93b1120184b6b6897f6a06c81039ea4b
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-02-03 18:08:16 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Georgi Nikolov on 03.02.22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
