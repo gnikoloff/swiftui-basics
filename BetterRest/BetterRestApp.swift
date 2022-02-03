a5ff4b290f521c2487a06d93756db2691277fd72
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-02-03 15:21:20 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Georgi Nikolov on 03.02.22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
