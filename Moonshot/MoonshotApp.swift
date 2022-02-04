a5cab36f1df098982fc9321b410108d53c064fa4
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-02-04 16:39:11 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Georgi Nikolov on 04.02.22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
